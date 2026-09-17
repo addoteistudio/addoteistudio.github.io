fb33df54bf4c99e0dc50f61c13fb48dcf5072e86
David Addotei <d8222815@gmail.com>
2026-09-17 10:03:31 +0000

    Bump cache version to v3.5 to force instant mobile cache refresh

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Addotei Creative Studio - Service Worker
-const CACHE_NAME = 'addotei-studio-v1';
+﻿// Addotei Creative Studio - Service Worker
+const CACHE_NAME = 'addotei-studio-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
