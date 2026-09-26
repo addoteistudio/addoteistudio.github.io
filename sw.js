f77ff590113b5cc5f847da8f88a2a68a6c35df8e
David Addotei <d8222815@gmail.com>
2026-09-26 09:55:35 +0000

    Remove Follow & Connect social column from footer (v4.7)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Addotei Creative Studio - Service Worker
-const CACHE_NAME = 'addotei-studio-v4-6';
+const CACHE_NAME = 'addotei-studio-v4-7';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
